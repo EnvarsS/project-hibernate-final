d5ca33577f2c73d64455ec168e5831f1a0d96d80
Vladyslav <eddiedipper4228@gmail.com>
2025-12-10 19:32:10 +0100

    Add DAO classes for City and Country

---
+++ src/main/java/org/hibernatecorp/projecthibernatefinal/DAO/CountryDAO.java
@@ -0,0 +1,20 @@
+package org.hibernatecorp.projecthibernatefinal.DAO;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.hibernatecorp.projecthibernatefinal.model.Country;
+
+import java.util.List;
+
+public class CountryDAO {
+    private final SessionFactory sessionFactory;
+
+    public CountryDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Country> getAll() {
+        Query<Country> query = sessionFactory.getCurrentSession().createQuery("select c from Country c", Country.class);
+        return query.list();
+    }
+}
